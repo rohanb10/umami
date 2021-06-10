7cb9b7c36f8a452d603d71a2fa0b3c9abf626db2
rohan <rohanb10@gmail.com>
2021-06-10 21:51:04 +0530

    snoozz + umami attempt 2

--- pages/_app.js
+++ pages/_app.js
@@ -33,14 +33,15 @@ export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Head>
-        <link rel="icon" href={`${basePath}/ext-favicon.ico`} />
-        <link rel="apple-touch-icon" sizes="180x180" href={`${basePath}/ext-apple-touch-icon.png`} />
-        <link rel="icon" type="image/png" sizes="32x32" href={`${basePath}/ext-icon-32.png`} />
-        <link rel="icon" type="image/png" sizes="16x16" href={`${basePath}/ext-icon-32.png`} />
+        <link rel="icon" href={`${basePath}/favicon.ico`} />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${basePath}/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${basePath}/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${basePath}/favicon-16x16.png`} />
         <link rel="manifest" href={`${basePath}/site.webmanifest`} />
+        <link rel="mask-icon" href={`${basePath}/safari-pinned-tab.svg`} color="#5bbad5" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#f3b845" />
+        <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Intl>
